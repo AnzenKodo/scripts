5bf7eea821f591cdb219d29c7fa0e93807052b12
AnzenKodo <50282743+AnzenKodo@users.noreply.github.com>
2026-05-31 12:19:31 +0530

    cleanup: remove description comments from script files

--- flameshot-start.sh
+++ flameshot-start.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
-# Description: Starts Flameshot in GUI mode, captures a screenshot, and copies it to the clipboard.
 
 /home/ramen/Applications/AppImages/flameshot gui --path /home/ramen/Pictures/Screenshots/ --raw | xclip -selection clipboard -t image/png
